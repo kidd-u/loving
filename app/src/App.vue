059d284cbe0ce1ac0352863ef4e1cd756c5458ea
Yuhang-Zheng <169012499+Yuhang-Zheng@users.noreply.github.com>
2025-05-12 13:28:07 +0800

    by

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,55 +1,73 @@
 <template>
-    <div id="app">
-        <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
-        <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
-    </div>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor
+      ref="resumeEditor"
+      :markdown="currentMarkdown"
+      :enableHtml="enableHtml"
+    ></ResumeEditor>
+  </div>
 </template>
 <script>
-import StyleEditor from './components/StyleEditor'
-import ResumeEditor from './components/ResumeEditor'
-import './assets/reset.css'
-let isPc = (function() {
-    var userAgentInfo = navigator.userAgent;
-    var Agents = ["Android", "iPhone",
-        "SymbianOS", "Windows Phone",
-        "iPad", "iPod"
-    ];
-    var flag = true;
-    for (var v = 0; v < Agents.length; v++) {
-        if (userAgentInfo.indexOf(Agents[v]) > 0) {
-            flag = false;
-            break;
-        }
+import StyleEditor from "./components/StyleEditor";
+import ResumeEditor from "./components/ResumeEditor";
+import "./assets/reset.css";
+let isPc = (function () {
+  var userAgentInfo = navigator.userAgent;
+  var Agents = [
+    "Android",
+    "iPhone",
+    "SymbianOS",
+    "Windows Phone",
+    "iPad",
+    "iPod",
+  ];
+  var flag = true;
+  for (var v = 0; v < Agents.length; v++) {
+    if (userAgentInfo.indexOf(Agents[v]) > 0) {
+      flag = false;
+      break;
     }
-    return flag;
-}());
-let getDateDiff = function(startDate, endDate) {
-    var startTime = new Date(Date.parse(startDate.replace(/-/g, "/"))).getTime();
-    var endTime = new Date(Date.parse(endDate.replace(/-/g, "/"))).getTime();
-    var dates = Math.abs((startTime - endTime)) / (1000 * 60 * 60 * 24);
-    return dates;
-}
-document.title += getDateDiff((new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1) + '-' + (new Date()).getDate(), '2016-09-15') + 1 + '天';
+  }
+  return flag;
+})();
+let getDateDiff = function (startDate, endDate) {
+  var startTime = new Date(Date.parse(startDate.replace(/-/g, "/"))).getTime();
+  var endTime = new Date(Date.parse(endDate.replace(/-/g, "/"))).getTime();
+  var dates = Math.abs(startTime - endTime) / (1000 * 60 * 60 * 24);
+  return dates;
+};
+document.title +=
+  getDateDiff(
+    new Date().getFullYear() +
+      "-" +
+      (new Date().getMonth() + 1) +
+      "-" +
+      new Date().getDate(),
+    "2016-09-15"
+  ) +
+  1 +
+  "天";
 export default {
-    name: 'app',
-    components: {
-        StyleEditor,
-        ResumeEditor
-    },
-    data() {
-        return {
-            interval: 27,
-            currentStyle: '',
-            enableHtml: false,
-            fullStyle: [
-                `/*
-* Hi。宝贝！
-* 这么久了。还没和宝贝说过我的工作呢！
-* 我是个前端工程师。俗称程序员。网页相关。
+  name: "app",
+  components: {
+    StyleEditor,
+    ResumeEditor,
+  },
+  data() {
+    return {
+      interval: 27,
+      currentStyle: "",
+      enableHtml: false,
+      fullStyle: [
+        `/*
+* Hi, YuanJiang!
+* 终于把域名配置好了。发现了一个好玩的东东！
+* 作为一名优秀的开发工程师。俗称程序员, 当然要部署了分享给你。
 * 如这个页面。就是个什么也没有的网页。
-* 我的工作就是给这种空白的页面加点儿东西。
+* 现在让我来给这种空白的页面加点儿东西。
 * 嗯。说起来手机和电脑还得区分一下。
-* 你现在用的是。。。${isPc ? '电脑' : '手机'}
+* 你现在用的是。。。${isPc ? "电脑" : "手机"}
 */
 
 /* 首先给所有元素加上过渡效果 */
@@ -68,7 +86,7 @@ html {
   border: 1px solid;
   margin: .5em;
   overflow: auto;
-  ${ isPc ? 'width: 48%;height: 96%;' : 'width: 96%;height: 50%;' }
+  ${isPc ? "width: 48%;height: 96%;" : "width: 96%;height: 50%;"}
   font-size: 14px;
   line-height:1.5;
 }
@@ -85,42 +103,56 @@ html{
 }
 .styleEditor {
   position: fixed; 
-  ${ isPc ? 'left: 0;' : 'left:0;right:0;margin:auto;'}
+  ${isPc ? "left: 0;" : "left:0;right:0;margin:auto;"}
   top: 0; 
   -webkit-transition: none; 
   transition: none;   
-  ${ isPc ? '-webkit-transform: rotateY(10deg) translateZ(-100px) ;transform: rotateY(10deg) translateZ(-100px) ;' : '-webkit-transform: rotateX(-10deg) translateZ(-100px) ;transform: rotateX(-10deg) translateZ(-100px) ;' }
-  ${ isPc ? '' : '-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;' }
+  ${
+    isPc
+      ? "-webkit-transform: rotateY(10deg) translateZ(-100px) ;transform: rotateY(10deg) translateZ(-100px) ;"
+      : "-webkit-transform: rotateX(-10deg) translateZ(-100px) ;transform: rotateX(-10deg) translateZ(-100px) ;"
+  }
+  ${
+    isPc ? "" : "-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;"
+  }
 }
 
 /* 再来一张信纸 */
 .resumeEditor{
   position: fixed; 
-  ${ isPc ? 'right: 0;' : 'left:0;right:0;margin:auto;'}
-  ${ isPc ? 'top: 0;' : 'bottom:2%;'}
+  ${isPc ? "right: 0;" : "left:0;right:0;margin:auto;"}
+  ${isPc ? "top: 0;" : "bottom:2%;"}
   padding: .5em;  
-  ${ isPc ? 'margin: .5em;' : ''}
-  ${ isPc ? 'width: 48%;height: 96%;' : 'width: 96%;height: 50%;' }
+  ${isPc ? "margin: .5em;" : ""}
+  ${isPc ? "width: 48%;height: 96%;" : "width: 96%;height: 50%;"}
   border: 1px solid;
   color: #222;
   overflow: auto;
   font-size: 14px;
   line-height:1.5;
-  ${ isPc ? '-webkit-transform: rotateY(-10deg) translateZ(-100px) ;transform: rotateY(-10deg) translateZ(-100px) ;' : '-webkit-transform: rotateX(10deg) translateZ(-100px) ;transform: rotateX(10deg) translateZ(-100px) ;' }
-    ${ isPc ? '' : '-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;' }
+  ${
+    isPc
+      ? "-webkit-transform: rotateY(-10deg) translateZ(-100px) ;transform: rotateY(-10deg) translateZ(-100px) ;"
+      : "-webkit-transform: rotateX(10deg) translateZ(-100px) ;transform: rotateX(10deg) translateZ(-100px) ;"
+  }
+    ${
+      isPc
+        ? ""
+        : "-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;"
+    }
   }
 /* 我开始写了 */
 
 
 `,
-                `
+        `
 /* 是不是看着很简陋粗糙？
  * 因为这是 Markdown 格式的
  * 一种程序员用来写文档日志的简易语言
  * 翻译成 网页 就行了
  */
 `,
-                `
+        `
 /* 再加点样式 */
 .resumeEditor{
   padding: 2em;
@@ -155,173 +187,123 @@ html{
 
 /* OK。完成！ */
 
-`
-            ],
-            currentMarkdown: '',
-            fullMarkdown: `nostar × juanjuan
+`,
+      ],
+      currentMarkdown: "",
+      fullMarkdown: `yvonne × kidd
 ----
 
-2016年08月05日。初初见面。两人齐齐心动。  
-2016年09月15日。即中秋节阴历八月十五。在一起。  
-已有 \`${getDateDiff((new Date()).getFullYear()+'-'+((new Date()).getMonth()+1)+'-'+(new Date()).getDate(),'2016-09-15') + 1}\` 天
+2012年08月31日。初次见面。高一14班报道。  
+去年在缘分的安排下，我们在一起了！  
+已经相识 \`${
+        getDateDiff(
+          new Date().getFullYear() +
+            "-" +
+            (new Date().getMonth() + 1) +
+            "-" +
+            new Date().getDate(),
+          "2012-08-31"
+        ) + 1
+      }\` 天
 
-一起呲过的餐厅
-----
+很开心收到来自三峡的明信片，这也是人生第一次收到信。哈哈哈，这将是我人生中最难忘记的回忆之一。
 
-* 昂吉拉姆西藏餐厅
-* 漫族餐厅
-* 鑫海汇海鲜烧烤
-* 老长沙罐子楼
-* 锅大侠火锅
-* 西湖春天
-* 天意
-* 曼玉
-* 王婆爱上虾
-* 十里洋场
-* 城墙根
-* 爱芳爱德
-* 阳光小店
-* 夜小红虾尾
-* 57°湘
-* 名厨味道
-* 老成都串串
-* 菜捕头
-* 知味观
-* 花港海航度假酒店
-* 冰城烧烤
-* ……
+也祝我的YuanJiang可以开心快乐每一天！要爱自己呀！！！
 
-一起看过的电影
-----
+> 【Screw the world × I have my dear YuanJiang】  
+> 喂。我不只想和你有当下。过去，现在，未来，我们都要在一起！
 
-1. 七月与安生
-2. 从你的全世界路过
-3. 驴得水
-4. 深海浩劫
-5. 湄公河行动
-6. 你的名字。
-7. 速度与激情8
-8. 战狼2
-9. 敦刻尔克
-10. 正义联盟
-11. 极盗车神
-12. ……
-13. 流浪地球
-14. 飞驰人生
-15. 新喜剧之王
-16. 来电狂响
-17. 疯狂的外星人
-18. 熊出没之原始时代
-19. ……
+`,
+    };
+  },
+  created() {
+    this.makeResume();
+  },
 
-一起玩过的地方
-----
-
-* 登封少林寺
-* 洛阳
-* 圣王坪
-* 大临淇
-* 皇城相府
-* 杭州西湖
-* ……
-
-一起玩过的游戏
-----
-
-1. 炉石传说
-2. 塞尔达传说-荒野之息
-3. 魂斗罗归来
-4. 马里奥奥德赛
-5. 欢乐麻将
-6. overcooked
-7. ……
-
-> 【Screw the world×I have my dear Juanjuan】  
-> 喂。我不只想影响你的习惯。我还要去改变你的人生。！
-
-`
-        }
+  methods: {
+    makeResume: async function () {
+      await this.progressivelyShowStyle(0);
+      await this.progressivelyShowResume();
+      await this.progressivelyShowStyle(1);
+      await this.showHtml();
+      await this.progressivelyShowStyle(2);
     },
-    created() {
-        this.makeResume()
+    showHtml: function () {
+      return new Promise((resolve, reject) => {
+        this.enableHtml = true;
+        resolve();
+      });
     },
-
-    methods: {
-        makeResume: async function() {
-            await this.progressivelyShowStyle(0)
-            await this.progressivelyShowResume()
-            await this.progressivelyShowStyle(1)
-            await this.showHtml()
-            await this.progressivelyShowStyle(2)
-        },
-        showHtml: function() {
-            return new Promise((resolve, reject) => {
-                this.enableHtml = true
-                resolve()
-            })
-        },
-        progressivelyShowStyle(n) {
-            return new Promise((resolve, reject) => {
-                let interval = this.interval
-                let showStyle = (async function() {
-                    let style = this.fullStyle[n]
-                    if (!style) {
-                        return
-                    }
-                    // 计算前 n 个 style 的字符总数
-                    let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
-                    let prefixLength = length - style.length
-                    if (this.currentStyle.length < length) {
-                        let l = this.currentStyle.length - prefixLength
-                        let char = style.substring(l, l + 1) || ' '
-                        this.currentStyle += char
-                        if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
-                            this.$nextTick(() => {
-                                this.$refs.styleEditor.goBottom()
-                            })
-                        }
-                        setTimeout(showStyle, interval)
-                    } else {
-                        resolve()
-                    }
-                }).bind(this)
-                showStyle()
-            })
-        },
-        progressivelyShowResume() {
-            return new Promise((resolve, reject) => {
-                let length = this.fullMarkdown.length
-                let interval = this.interval
-                let showResume = () => {
-                    if (this.currentMarkdown.length < length) {
-                        this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
-                        let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
-                        let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
-                        if (prevChar === '\n' && this.$refs.resumeEditor) {
-                            this.$nextTick(() => this.$refs.resumeEditor.goBottom())
-                        }
-                        setTimeout(showResume, interval)
-                    } else {
-                        resolve()
-                    }
-                }
-                showResume()
-            })
-        }
-    }
-}
+    progressivelyShowStyle(n) {
+      return new Promise((resolve, reject) => {
+        let interval = this.interval;
+        let showStyle = async function () {
+          let style = this.fullStyle[n];
+          if (!style) {
+            return;
+          }
+          // 计算前 n 个 style 的字符总数
+          let length = this.fullStyle
+            .filter((_, index) => index <= n)
+            .map((item) => item.length)
+            .reduce((p, c) => p + c, 0);
+          let prefixLength = length - style.length;
+          if (this.currentStyle.length < length) {
+            let l = this.currentStyle.length - prefixLength;
+            let char = style.substring(l, l + 1) || " ";
+            this.currentStyle += char;
+            if (style.substring(l - 1, l) === "\n" && this.$refs.styleEditor) {
+              this.$nextTick(() => {
+                this.$refs.styleEditor.goBottom();
+              });
+            }
+            setTimeout(showStyle, interval);
+          } else {
+            resolve();
+          }
+        }.bind(this);
+        showStyle();
+      });
+    },
+    progressivelyShowResume() {
+      return new Promise((resolve, reject) => {
+        let length = this.fullMarkdown.length;
+        let interval = this.interval;
+        let showResume = () => {
+          if (this.currentMarkdown.length < length) {
+            this.currentMarkdown = this.fullMarkdown.substring(
+              0,
+              this.currentMarkdown.length + 1
+            );
+            let lastChar =
+              this.currentMarkdown[this.currentMarkdown.length - 1];
+            let prevChar =
+              this.currentMarkdown[this.currentMarkdown.length - 2];
+            if (prevChar === "\n" && this.$refs.resumeEditor) {
+              this.$nextTick(() => this.$refs.resumeEditor.goBottom());
+            }
+            setTimeout(showResume, interval);
+          } else {
+            resolve();
+          }
+        };
+        showResume();
+      });
+    },
+  },
+};
 </script>
 <style scoped>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 html {
-    min-height: 100%;
+  min-height: 100%;
 }
 .styleEditor {
-    -webkit-backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
 }
 </style>
